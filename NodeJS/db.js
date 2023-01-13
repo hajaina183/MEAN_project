2fec053d2e25c43e3c23d43c0deb4ef55c0611ff
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-13 20:14:19 +0300

    create repository for nodejs

---
+++ NodeJS/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://hajaina:mdpprom13@cluster0.rcc3ot6.mongodb.net/CrudDB', (err) => {
+    if (!err)
+        console.log('MongoDB connection succeeded.');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
