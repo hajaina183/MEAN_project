d1d07106ab4b7e82337d95497efc3cce4bdcab74
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-14 00:36:04 +0300

    update clientController

--- NodeJS/controllers/clientController.js
+++ NodeJS/controllers/clientController.js
@@ -3,27 +3,64 @@ var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 
 var { Client } = require('../models/client');
-console.log("aaaaaaaaaaaaaaaa");
 
+// => localhost:3000/admin/
+router.get('/', (req, res) => {
+    Client.find((err, docs) => {
+        if (!err) { res.send(docs); }
+        else { console.log('Error in Retriving Clients :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+        Client.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Retriving Client :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
 router.post('/', (req, res) => {
-    console.log("tongaaaaaaa");
-    var emp = new Client({
+    var cli = new Client({
         nom: req.body.nom,
         prenom: req.body.prenom,
         adresse: req.body.adresse,
         tel: req.body.tel,
-        email: req.body.email,
         mdp: req.body.mdp,
     });
-    
-    console.log(req.body.nom);
-    emp.save((err, doc) => {
+    cli.save((err, doc) => {
         if (!err) { res.send(doc); }
-        else { console.log('Error in Employee Save :' + JSON.stringify(err, undefined, 2)); }
+        else { console.log('Error in Client Save :' + JSON.stringify(err, undefined, 2)); }
     });
 });
 
+router.put('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
 
+    var cli = {
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        adresse: req.body.adresse,
+        tel: req.body.tel,
+        mdp: req.body.mdp,
+    };
+    Client.findByIdAndUpdate(req.params.id, { $set: cli }, { new: true }, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Client Update :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
+
+    Client.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Client Delete :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
 module.exports = router;
