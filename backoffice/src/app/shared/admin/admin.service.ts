46822167bb33b8d034c3c8601c1fda2bd2a48366
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-14 00:45:44 +0300

    shared admin

---
+++ backoffice/src/app/shared/admin/admin.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+import { Admin } from './admin.model';
+
+@Injectable()
+export class AdminService {
+  selectedAdmin: Admin;
+  admins: Admin[];
+  readonly baseURL = 'http://localhost:3000/admin';
+
+  constructor(private http: HttpClient) { }
+
+  postAdmin(emp: Admin) {
+    return this.http.post(this.baseURL, emp);
+  }
+
+  getAdminList() {
+    return this.http.get(this.baseURL);
+  }
+
+  putAdmin(emp: Admin) {
+    return this.http.put(this.baseURL + `/${emp._id}`, emp);
+  }
+
+  deleteAdmin(_id: string) {
+    return this.http.delete(this.baseURL + `/${_id}`);
+  }
+
+}
