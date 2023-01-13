0fa211da6770fe89efc31e1db5124b994a864803
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-13 23:46:41 +0300

    login backoffice frontend

--- backoffice/src/app/app.routing.ts
+++ backoffice/src/app/app.routing.ts
@@ -4,23 +4,107 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
 
 const routes: Routes =[
+  { path: "", component: LoginComponent},
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
-    path: '',
+    path: 'acceuil',
     component: AdminLayoutComponent,
     children: [
-        {
-      path: '',
-      loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(x=>x.AdminLayoutModule)
-  }]},
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+    ],
+  },
   {
-    path: '**',
-    redirectTo: 'dashboard'
+    path: 'dashboard',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+    ],
+  },
+  {
+    path: 'user-profile',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: UserProfileComponent,
+      },
+    ],
+  },
+  { 
+    path: 'table-list',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: TableListComponent,
+      },
+    ], 
+  },
+  { 
+    path: 'typography',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: TypographyComponent,
+      },
+    ], 
+  },
+  { 
+    path: 'icons',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: IconsComponent,
+      },
+    ], 
+  },
+  { 
+    path: 'maps',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: MapsComponent,
+      },
+    ], 
+  },
+  { 
+    path: 'notifications',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: NotificationsComponent,
+      },
+    ], 
+  },
+  { 
+    path: 'upgrade',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: UpgradeComponent,
+      },
+    ], 
   }
 ];
 
