2dbe6733187f402d756efb5cfa9a79e7527cd8ca
AnitaRak <anitatantely@gmail.com>
2023-01-13 23:00:31 +0300

    front-Office(Login et template)

---
+++ frontOffice/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: "", component: NavbarComponent},
+  { path: "login", component: LoginComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
