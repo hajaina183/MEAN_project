87c3ca66e9d40f0a566029bbf50471162afd80f5
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-15 21:39:00 +0300

    update url cors

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -9,7 +9,7 @@ var adminController = require('./controllers/adminController.js');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(cors({ origin: '*' }));
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
