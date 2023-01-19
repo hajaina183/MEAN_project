1dd3a1cc5fa804895b066cca22d737828ed8f48b
AnitaRak <anitatantely@gmail.com>
2023-01-19 23:14:21 +0300

    insertVoiture

--- NodeJS/controllers/reparationVoitureController.js
+++ NodeJS/controllers/reparationVoitureController.js
@@ -5,6 +5,24 @@ var ObjectId = require('mongoose').Types.ObjectId;
 var { ReparationVoiture } = require('../models/reparationVoiture');
 
 // => localhost:3000/admin/
+router.route("/find").get(function(req, res) {
+    ReparationVoiture.find({}, function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        var comm;
+        var repVoiture = Array;
+        for (var i = 0, l = result.length; i < l; i++) {
+            comm = result[i];
+            console.log('------------------------------');
+            console.log('nom : ' + comm.voiture.length);
+            console.log('------------------------------');
+        }
+        res.json(result);
+      }
+    });
+  });
+
 router.get('/', (req, res) => {
     ReparationVoiture.find((err, docs) => {
         if (!err) { res.send(docs); }
@@ -47,17 +65,9 @@ router.post('/insertPersoVoiture', (req, res) => {
     const updateDoc = {
         $push:{
             voiture:{
-                modele: "BMW",
-                numero: "1236wwt",
-                diagnostique: 1,
-                reparation: [
-                    {
-                        daty: "2023-47-7",
-                        type: "pneu",
-                        prix: 25000,
-                        etat: 0
-                    }
-                ]
+                modele: "Mercedes AMG",
+                numero: "2345 TBJ",
+                diagnostique: 0,
             }
         },
       };
@@ -71,7 +81,47 @@ router.post('/insertPersoVoiture', (req, res) => {
         });
 });*/
 
-
+router.put('/insertVoitureReparation', (req, res) => {
+    console.log(req.body.voiture[0].modele);
+    var voi = new ReparationVoiture({
+        nom: req.body.nom,
+        email: req.body.email,
+        voiture: req.body.voiture
+    });
+    const filter = { nom : req.body.nom , email: req.body.email , "voiture.modele": req.body.voiture[0].modele, "voiture.numero": req.body.voiture[0].numero };
+    const updateDoc = {
+        $push:{
+            "voiture.$.reparation": 
+                {
+                    daty: "2023-18-1",
+                    type: "embrayage",
+                    prix: 100000,
+                    etat: 0
+                }    
+        },
+      };
+      const updateDoc2 = {
+        $set:{
+            "voiture.$.diagnostique": 1 
+        },
+      };
+        ReparationVoiture.updateOne(filter, updateDoc, function (err, docs) {
+            if (err){
+                console.log(err);
+            }
+            else{
+                ReparationVoiture.updateOne(filter, updateDoc2, function (err1, doc) {
+                    if (err1){
+                        console.log(err1);
+                    }
+                    else{
+                        res.send(docs);
+                    }
+                });
+            }
+        });
+        
+});
 
 router.delete('/:id', (req, res) => {
     if (!ObjectId.isValid(req.params.id))
