132eb0553c07ab1e77fa2960d755422eb91fb2ac
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-29 00:20:08 +0300

    debug HTTPS in server

--- frontOffice/src/app/app.module.ts
+++ frontOffice/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -15,6 +15,7 @@ import { PaiementComponent } from './paiement/paiement.component';
 import { PagedirigeantComponent } from './pagedirigeant/pagedirigeant.component';
 import { HistoriqueComponent } from './historique/historique.component';
 import { FooterComponent } from './footer/footer.component';
+import { NoopInterceptor } from './NoopInterceptor..interceptor';
 
 
 @NgModule({
@@ -39,7 +40,9 @@ import { FooterComponent } from './footer/footer.component';
     ReactiveFormsModule,
     ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
