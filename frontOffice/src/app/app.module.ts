3017af853eb2352f01f4fa14e87d3264a41b4a9f
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2023-01-29 01:38:49 +0300

    Revert "debug HTTPS in server"
    This reverts commit 132eb0553c07ab1e77fa2960d755422eb91fb2ac.

--- frontOffice/src/app/app.module.ts
+++ frontOffice/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -15,7 +15,6 @@ import { PaiementComponent } from './paiement/paiement.component';
 import { PagedirigeantComponent } from './pagedirigeant/pagedirigeant.component';
 import { HistoriqueComponent } from './historique/historique.component';
 import { FooterComponent } from './footer/footer.component';
-import { NoopInterceptor } from './NoopInterceptor..interceptor';
 
 
 @NgModule({
@@ -40,9 +39,7 @@ import { NoopInterceptor } from './NoopInterceptor..interceptor';
     ReactiveFormsModule,
     ToastrModule.forRoot()
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
